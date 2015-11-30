8789bd40c9ee87691c1a134845bfc42397bd8c6b
DENRYU <tdggame1a23@gmail.com>
2015-11-30 23:57:32 +0900

    ボタン用スクリプトの追加、ただしまだ最適化の余地アリ

---
+++ 2DCMU/Assets/Script/ButtonS1.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonS1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ButanC(){
+		Debug.Log ("ボタンクリック1!");
+	}
+
+	public void SceneLoad(){
+		Application.LoadLevel(Application.loadedLevel);
+	}
+}
